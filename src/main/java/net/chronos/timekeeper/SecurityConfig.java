ff8d6c59eee6b20c5bd91b07a4e83d2a91027100
dgagne13David Gagne <djgagne01@yahoo.com>
2015-11-10 08:14:51 -0500

    Add login page and security profile. add error page

---
+++ src/main/java/net/chronos/timekeeper/SecurityConfig.java
@@ -0,0 +1,35 @@
+package net.chronos.timekeeper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //TODO: enable csrf and include token in request headers
+        http    .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("machoman").password("ohyeah").roles("HRMANAGER");
+    }
+
+
+}
